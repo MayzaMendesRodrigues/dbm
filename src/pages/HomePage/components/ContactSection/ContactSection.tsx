f87ea970a94a5597621c5724145f596ec741ede8
Jonas Bazzi <jonasbazzi16@gmail.com>
2025-08-20 00:13:12 +0200

    fix: contact buttons mobile

--- src/pages/HomePage/components/ContactSection/ContactSection.tsx
+++ src/pages/HomePage/components/ContactSection/ContactSection.tsx
@@ -11,16 +11,18 @@ const ContactSection: React.FC = () => {
           Contactanos ahora para agendar una prueba de manejo o recibir
           asesoramiento personalizado.
         </p>
-        <Button href="tel:+5491173608326"
-          onClick={() => pushEvent(EventAnalytics.FooterCallButton)}>
-          <i className="fas fa-phone-alt"></i>Llamar ahora
-        </Button>
-        <Button variant="outline"
-          href="https://wa.me/5491173608326?text=Hola,%20quiero%20información%20sobre%20sus%20motos"
-          onClick={() => pushEvent(EventAnalytics.FooterWhatsappButton)}
-          style={{ marginLeft: '20px' }}>
-          <i className="fab fa-whatsapp"></i>WhatsApp
-        </Button>
+        <div className="contact-btns">
+          <Button
+            href="tel:+5491173608326"
+            onClick={() => pushEvent(EventAnalytics.FooterCallButton)}>
+            <i className="fas fa-phone-alt"></i>Llamar ahora
+          </Button>
+          <Button variant="outline"
+            href="https://wa.me/5491173608326?text=Hola,%20quiero%20información%20sobre%20sus%20motos"
+            onClick={() => pushEvent(EventAnalytics.FooterWhatsappButton)}>
+            <i className="fab fa-whatsapp"></i>WhatsApp
+          </Button>
+        </div>
       </div>
     </section >
   )
